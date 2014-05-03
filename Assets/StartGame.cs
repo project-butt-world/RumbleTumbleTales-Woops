048ec845eb48b125bf073df0fb39477fd596a3e5
GallagherJ <gallagherj7@gmail.com>
2014-05-03 22:53:17 +0100

    structure sorted
    yo

---
+++ Assets/StartGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	void OnBegin( TapGesture gesture ) 
+	{
+		if( gesture.Selection )
+			Debug.Log( "Tapped object: " + gesture.Selection.name );
+		AutoFade.LoadLevel(1 ,3,1,Color.black);
+		//	    Application.LoadLevel (1);
+
+		//		else
+		//			Debug.Log( "No object was tapped at " + gesture.Position );
+		//	}
+		//	Debug.Log( "Tap gesture detected at " + gesture.Position + 
+		//		           ". It was sent by " + gesture.Recognizer.name );
+		
+	}
+}
