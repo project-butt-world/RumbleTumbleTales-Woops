5191d46256a8399bab7fa4969383f883f5cf9e8e
Jonathan Gallagher <gallagherj7@gmail.com>
2014-05-03 17:46:22 +0100

    touchnworkin
    yaaah

---
+++ Assets/DoStuffOnTap.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoStuffOnTap : MonoBehaviour {
+
+	void OnTap( TapGesture gesture ) 
+	{
+		if( gesture.Selection )
+			Debug.Log( "Tapped object: " + gesture.Selection.name );
+		    this.transform.Translate(Vector2.up * Time.deltaTime, Space.World);
+//		else
+//			Debug.Log( "No object was tapped at " + gesture.Position );
+//	}
+	//	Debug.Log( "Tap gesture detected at " + gesture.Position + 
+//		           ". It was sent by " + gesture.Recognizer.name );
+
+}
+}
