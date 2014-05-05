a654605b8f2b39b006822a1975da0dfccbeb5773
GallagherJ <gallagherj7@gmail.com>
2014-05-05 22:35:46 +0100

    before trying to swipe books
    arg

---
+++ Assets/swipebooks.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class swipebooks : MonoBehaviour {
+
+  void OnSwipe( SwipeGesture gesture )
+	{
+		if( gesture.StartSelection && gesture.StartSelection.rigidbody )
+		{
+			Vector2 swipeMotion = gesture.Move;
+			float speedScale = 7.0f;
+			Vector3 force = new Vector3( speedScale * swipeMotion.x, 0, 0 );
+			
+			// apply the force on the physic object
+			gesture.StartSelection.rigidbody.AddForce( force );
+		}
+	}
+
+	}
+
+
+
