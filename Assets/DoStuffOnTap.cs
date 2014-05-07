40bac0d3ed5574eeedfcf054d9bd3afaaf33b00c
GallagherJ <gallagherj7@gmail.com>
2014-05-07 01:17:05 +0100

    doors done
    dooooors

--- Assets/DoStuffOnTap.cs
+++ Assets/DoStuffOnTap.cs
@@ -3,11 +3,37 @@ using System.Collections;
 
 public class DoStuffOnTap : MonoBehaviour {
 
+	public Animator dooranim;
+	public bool tapped;
+
+	void Start() {
+		dooranim = GetComponent<Animator>();
+		tapped = false;
+
+	}
+
+	void Update(){
+
+	//	if (tapped = true) {
+		//	Debug.Log("tapped");
+					//	dooranim.Play ("DoorOpenYellow");
+				}
+		
+//	void OnFingerDown(FingerDownEvent e)
 	void OnTap( TapGesture gesture ) 
 	{
-		if( gesture.Selection )
+		if( gesture.Selection)
 			Debug.Log( "Tapped object: " + gesture.Selection.name );
-		    this.transform.Translate(Vector2.up * Time.deltaTime, Space.World);
+		  //  dooranim.Play ("DoorOpenYellow");
+		      dooranim.CrossFade("DoorOpenYellow", 0.9F);
+		Object.Destroy(this, 0.9f);
+		     // tapped = true;
+		   // this.transform.Translate(Vector2.up * Time.deltaTime, Space.World);
+		//    dooranim.CrossFade("DoorOpenYellow", 1.0);
+				    
+	//	WaitForSeconds (3);
+
+		//	    dooranim.SetBool("open",true);
 //		else
 //			Debug.Log( "No object was tapped at " + gesture.Position );
 //	}
@@ -15,4 +41,6 @@ public class DoStuffOnTap : MonoBehaviour {
 //		           ". It was sent by " + gesture.Recognizer.name );
 
 }
+
+
 }
