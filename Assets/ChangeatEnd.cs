0b51b008695091ddb345f0466b2540bfa2f9c743
GallagherJ <gallagherj7@gmail.com>
2014-05-08 19:25:30 +0100

    skip and again text
    blerns

---
+++ Assets/ChangeatEnd.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeatEnd : MonoBehaviour {
+
+	void Start(){
+
+		changez ();
+		}
+
+void changez(){
+				AutoFade.LoadLevel (2, 3, 1, Color.black);
+		}
+}
